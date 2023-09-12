adeedcee9b8bdb01642df01a755ed0abbf68da3f
KhuatCong <khuatphucong06092002@gmail.com>
2023-09-12 15:36:34 +0700

    update

--- main.js
+++ main.js
@@ -20,25 +20,34 @@ if ($('#back-to-top').length) {
     });
 }
 
-$(document).ready(function(){
+$(document).ready(function () {
     $('.owl-carousel').owlCarousel({
-        loop:true,
-        margin:10,
-        dots:false,
-        nav:true,
-        autoplay:true,
-        autoplayTimeout:2000,
-        autoplayHoverPause:true,
-        responsive:{
-            0:{
-                items:2
+        loop: true,
+        margin: 10,
+        dots: false,
+        nav: true,
+        autoplay: true,
+        autoplayTimeout: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 2
             },
-            600:{
-                items:3
+            600: {
+                items: 3
             },
-            1000:{
-                items:5
+            1000: {
+                items: 5
             }
         }
-    })
+    });
+//     var owl = $('.owl-carousel');
+//     owl.on('mousewheel', '.owl-stage', function (e) {
+//         if (e.deltaY > 0) {
+//             owl.trigger('next.owl');
+//         } else {
+//             owl.trigger('prev.owl');
+//         }
+//         e.preventDefault();
+//     });
 })
